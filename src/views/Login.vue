534c6c01a884e3e792d73e01b1b0d36ddc8183f1
EdwardSPO <sp410110@gmail.com>
2022-11-07 12:02:25 -0500

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,31 @@
 <template>
-<div>
-  <br/>
- <v-card class="mx-auto"  max-width="444" >
-
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-card-text>
-       <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-    <v-text-field v-model="password"  :rules="paswwordRules" label="Password" required ></v-text-field>
-   <v-btn  :disabled="!valid"  color="success"   class="mr-4"  @click="validate">Login</v-btn>
-
-    </v-card-text>
-  </v-form>
-  </v-card>
-</div>
+   <v-app id="inspire">
+      <v-content>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Login form</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                          <v-form ref="form" v-model="valid" lazy-validation>
+                           <v-text-field prepend-icon="person" v-model="email" :rules="emailRules" name="login" label="Login" type="text" required></v-text-field>
+                           <v-text-field id="password"  v-model="password"  :rules="paswwordRules" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn  color="primary" @click="validate">Login</v-btn>
+                     </v-card-actions>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-content>
+   </v-app>
 </template>
+
 <script>
 import axios from "axios";
 import swal from 'sweetalert';
@@ -65,3 +73,4 @@ import swal from 'sweetalert';
     },
   }
 </script>
+
